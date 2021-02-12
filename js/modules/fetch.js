9dac12c47ab56d16618eaff6408897993e96f18f
Jochem Vogel <voge0031@hz.nl>
2021-02-12 16:19:44 +0100

    Add .editorconfig
    Trying to fix the massive indenting on GitHub. Not sure if this will work

--- js/modules/fetch.js
+++ js/modules/fetch.js
@@ -1,13 +1,13 @@
-import { API_KEY, API_HOST } from '../constants/api.js';
+import { API_KEY, API_HOST } from "../constants/api.js";
 
 export { getResults };
 
 const headers = {
-	method: 'GET',
-	headers: {
-		'x-rapidapi-key': API_KEY,
-		'x-rapidapi-host': API_HOST,
-	},
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": API_KEY,
+    "x-rapidapi-host": API_HOST,
+  },
 };
 
 /**
@@ -16,30 +16,30 @@ const headers = {
  * @returns {Object} data - All the data that is fetched by the API
  */
 async function getResults(url) {
-	return fetch(url, headers)
-		.then(function (response) {
-			if (!response.ok) {
-				if (response.status === 429) {
-					alert('Too many request. Wait a second');
-					throw Error(response.statusText);
-				}
+  return fetch(url, headers)
+    .then(function (response) {
+      if (!response.ok) {
+        if (response.status === 429) {
+          alert("Too many request. Wait a second");
+          throw Error(response.statusText);
+        }
 
-				if (response.status === 401) {
-					alert(
-						'There is something wrong with the API key. Please contact the admin.'
-					);
-					throw Error(response.statusText);
-				}
+        if (response.status === 401) {
+          alert(
+            "There is something wrong with the API key. Please contact the admin."
+          );
+          throw Error(response.statusText);
+        }
 
-				throw Error(response.statusText);
-			}
+        throw Error(response.statusText);
+      }
 
-			return response.json();
-		})
-		.then(function (data) {
-			return data;
-		})
-		.catch(function (error) {
-			console.log(error);
-		});
+      return response.json();
+    })
+    .then(function (data) {
+      return data;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
