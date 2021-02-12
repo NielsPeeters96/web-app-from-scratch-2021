517bb5ef10a60380dd21297f6d998b8e71553928
Jochem Vogel <voge0031@hz.nl>
2021-02-12 14:18:50 +0100

    Add some reponse.status checks
    Only for a 429 & 401. The others are handled throughout the rest of the application. statusText will appear on every reponse that is not 200,  anyways

--- js/modules/fetch.js
+++ js/modules/fetch.js
@@ -19,7 +19,18 @@ async function getResults(url) {
 	return fetch(url, headers)
 		.then(function (response) {
 			if (!response.ok) {
-				// @TODO Make difference between 400, 401 & 404
+				if (response.status === 429) {
+					alert('Too many request. Wait a second');
+					throw Error(response.statusText);
+				}
+
+				if (response.status === 401) {
+					alert(
+						'There is something wrong with the API key. Please contact the admin.'
+					);
+					throw Error(response.statusText);
+				}
+
 				throw Error(response.statusText);
 			}
 
